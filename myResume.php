23562f41ec11887009855485d535e643514601b6
AniamK <kathris@hotmail.co.th>
2018-06-21 21:23:27 +0700

    add myResume.php

---
+++ myResume.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>My Resume</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <!--CSS only-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <!--JS, Popper.js, and jQuery-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <!-- end head -->
+</head>
+<body>
+    <?php
+    include "menuBar.php";
+    ?>
+    <div class="container">
+        <div class="container" style="text-align: center;">
+            Mr.Apiwut Trusphimai<br>
+            163/1 Moo 5 Sumrith<br>
+            Phimai Nakhon Rachasima 30110<br>
+            Tel: 0985932024 Email: apiwut_t@kkumail.com<br>
+            Age: 23<br>
+            Marital Status: Single<br>
+            Hobbies: Reading, Watching Movie, Listening to Music<br>
+            Expected Salary: 19,000 – 23,000 Baht<br>
+        </div><br><br>
+    <!-- Education-->
+        <h4>Education</h4>
+        <ul>
+            <li><p><b>Khon Kaen University Bachelor of Science, Major in Computer Science</b> 2014-2018</p></li>
+        </ul><br>
+    <!-- Technology Skills Summary -->
+        <h4>Technology Skills Summary</h4>
+        <ul>
+            <li><p><b>Programming/Languages:</b> Java, C, Python, PHP, HTML</p></li>
+            <li><p><b>Source Code Control:</b> Git</p></li>
+            <li><p><b>Databases:</b> MySQL</p></li>
+            <li><p><b>Design &amp; IDE Tools:</b> Moqup</p></li>
+            <li><p><b>Office Tools:</b> MS Word, Excel, PowerPoint</p></li>
+            <li><p><b>Operating Systems:</b> Windows XP/7/8/10, Ubuntu 12</p></li>
+        </ul><br>
+    <!-- Intership -->
+        <h4>Internship</h4>
+        <ul>
+            <li><p><b>Bureau of Information Technology Khon Kaen University</b> June 5, 2018 to August 1, 2018 </p></li>
+        </ul><br>
+    <!-- Awards / Certificate -->
+        <h4>Certificate</h4>
+            <ul>
+                <li><p><b>The 6th ASEAN Undergraduate Conference in Computing (AUCC 2018)</b></p></li>
+            </ul><br>
+        </div>
+</body>
+</html>
